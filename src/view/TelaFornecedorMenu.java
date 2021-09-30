0c0cfbfac37bb70c4ef2db61b48466c78db4038a
Juan Jose Cerqueira de Sousa <64857101+cerqueirajuan@users.noreply.github.com>
2021-09-30 14:32:12 -0300

    fornecedor
    Adicionadas as classes de view referentes ao fornecedor

---
+++ src/view/TelaFornecedorMenu.java
@@ -0,0 +1,41 @@
+package view;
+
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.*;
+
+public class TelaFornecedorMenu {
+	private static JFrame principalFornecedor = new JFrame("Loja de Óculos");
+	private static JLabel titulo = new JLabel("Fornecedores");
+	private static JButton visualizar = new JButton("Visualizar");
+	private static JButton cadastrar = new JButton("Cadastrar");
+	private static JButton alterar = new JButton("Alterar");
+	private static JButton deletar = new JButton("Deletar");
+	private static JButton buscar = new JButton("Buscar");
+	
+	public TelaFornecedorMenu() {
+		titulo.setFont(new Font("Arial", Font.BOLD, 20));
+		titulo.setBounds(125, 10, 150, 30);
+		
+		principalFornecedor.add(titulo);
+		principalFornecedor.add(visualizar);
+		principalFornecedor.add(cadastrar);
+		principalFornecedor.add(alterar);
+		principalFornecedor.add(deletar);
+		principalFornecedor.add(buscar);
+		
+		visualizar.setBounds(140, 50, 100, 30);
+		cadastrar.setBounds(140, 100, 100, 30);
+		alterar.setBounds(140, 150, 100, 30);
+		deletar.setBounds(140, 200, 100, 30);
+		buscar.setBounds(140, 250, 100, 30);
+		
+		principalFornecedor.setLayout(null);
+
+		principalFornecedor.setSize(400, 350);
+		principalFornecedor.setVisible(true);
+	}
+	
+}
--- src/view/TelaPessoa.java
+++ src/view/TelaPessoa.java
@@ -11,6 +11,7 @@ public class TelaPessoa implements ActionListener {
 	private static JLabel titulo = new JLabel("Pessoas");
 	private static JButton funcionario = new JButton("Funcionários");
 	private static JButton cliente = new JButton("Clientes");
+	private static JButton fornecedor = new JButton("Fornecedores");
 
 	// Construtor de tela das opções de funcionários e clientes
 
@@ -21,9 +22,11 @@ public class TelaPessoa implements ActionListener {
 		principalPessoa.add(titulo);
 		principalPessoa.add(funcionario);
 		principalPessoa.add(cliente);
+		principalPessoa.add(fornecedor);
 		
 		funcionario.setBounds(90, 50, 200, 30);
 		cliente.setBounds(90, 100, 200, 30);
+		fornecedor.setBounds(90, 150, 200, 30);
 		
 		principalPessoa.setLayout(null);
 
@@ -36,6 +39,7 @@ public class TelaPessoa implements ActionListener {
 		
 		funcionario.addActionListener(menu);
 		cliente.addActionListener(menu);
+		fornecedor.addActionListener(menu);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
@@ -49,5 +53,8 @@ public class TelaPessoa implements ActionListener {
 			new TelaClienteMenu();
 		}
 		
+		if(src == fornecedor) {
+			new TelaFornecedorMenu();
+		}
 	}
 }
