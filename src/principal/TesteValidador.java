31cbca65595c1b8a9b30de191508ea4870ee262e
Juan Jose Cerqueira de Sousa <64857101+cerqueirajuan@users.noreply.github.com>
2021-10-08 15:23:37 -0300

    Javadoc teste e validação
    Classes de teste e validação documentadas

--- src/principal/TesteValidador.java
+++ src/principal/TesteValidador.java
@@ -1,5 +1,10 @@
 package principal;
 
+/**
+ * Implementa os testes unitários do método Validador
+ * @author Mateus Vinícius
+ * @version 1.0 (Out 2021)
+ */
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -8,20 +13,38 @@ class TesteValidador {
 	
 	Validador v = new Validador();
 
+	/**
+	 * Testa se o método de validação do nome de usuário está funcionando
+	 * O campo de texto não pode ser nulo
+	 * @author Mateus Vinícius
+	 * @version 1.0 (Out 2021)
+	 */
 	@Test
 	void testValidaNomeCliente() {
-		assertTrue(v.validaNomeCliente("Mateus")); //retorna true -> passa no teste
-		assertTrue(v.validaNomeCliente("")); 	  // retorna false -> não passa no teste
-		assertFalse(v.validaNomeCliente("Joaquim")); // retorna true -> não passa no teste
+		assertTrue(v.validaNomeCliente("Mateus")); 
+		assertTrue(v.validaNomeCliente("")); 	  
+		assertFalse(v.validaNomeCliente("Joaquim")); 
 	}
 
+	/**
+	 * Testa se o método de validação da quantidade vendida está funcionando
+	 * O valor preenchido não pode ser igual a 0
+	 * @author Mateus Vinícius
+	 * @version 1.0 (Out 2021)
+	 */
 	@Test
 	void testValidaQuantidadeVenda() {
-		assertEquals(true, 10);  //retorna true -> passa no teste
-		assertEquals(true, 0); //retorna false -> não passa no teste
-		assertTrue(v.validaQuantidadeVenda(15)); //retorna true -> passa no teste
+		assertEquals(true, 10);
+		assertEquals(true, 0); 
+		assertTrue(v.validaQuantidadeVenda(15));
 	}
 
+	/**
+	 * Testa se o telefone digitado possui caracteres não numéricos
+	 * O campo de telefone deve receber apenas números
+	 * @author Mateus Vinícius
+	 * @version 1.0 (Out 2021)
+	 */
 	@Test
 	void testValidaTelefone() {
 		assertEquals(true, "99985-7456");  //retorna true -> passa no teste
--- src/principal/Validador.java
+++ src/principal/Validador.java
@@ -1,9 +1,24 @@
 package principal;
 
+/**
+ * Valida os seguintes dados preenchidos pelo usuário:
+ * (1) Nome do cliente
+ * (2) Quantidade vendida
+ * (3) Telefone
+ * @author Mateus Vinícius
+ * @version 1.0 (Out 2021)
+ */
 public class Validador {
 	
 
-	
+	/**
+	 * Checa se o campo de texto está vazio
+	 * @param nome uma string que contém o nome do cliente a ser checado
+	 * @return true caso o campo de texto não esteja vazio
+	 * @return false caso o campo de texto esteja vazio
+	 * @author Juan Cerqueira
+	 * @version 1.0 (Out 2021)
+	 */
 	public boolean validaNomeCliente(String nome) {
 		boolean aux = true;
 		
@@ -15,6 +30,14 @@ public class Validador {
 		return aux;
 	}
 	
+	/**
+	 * Checa se a quantidade vendida de um produto é maior que zero
+	 * @param quantidadeVenda um int que contém a quantidade vendida
+	 * @return false caso a quantidade inserida seja igual a 0
+	 * @return true caso a quantidade inserida seja diferente de 0
+	 * @author Juan Cerqueira
+	 * @version 1.0 (Out 2021)
+	 */
 	public boolean validaQuantidadeVenda(int quantidadeVenda) {
 		
 		boolean aux = true;
@@ -28,6 +51,14 @@ public class Validador {
 		return aux;
 	}
 	
+	/**
+	 * Checa se o telefone digitado possui caracteres não numéricos
+	 * @param telefone uma string que contém o número de contato
+	 * @return false caso o telefone digitado possua "@" ou "!"
+	 * @return true caso o telefone possua apenas números
+	 * @author Mateus Vinícius
+	 * @version 1.0 (Out 2021)
+	 */
 	public boolean validaTelefone(String telefone) {
 		boolean aux = true;
 		
